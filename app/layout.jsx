f785668397c9b0ef05d0bf3c2f912fdfd9a4604f
Barney <liubomir60@gmail.com>
2023-10-04 12:54:39 +0300

    Added analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Layout } from '@/components/dom/Layout'
 import '@/global.css'
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: 'Next.js + Three.js',
@@ -16,7 +17,10 @@ export default function RootLayout({ children }) {
       <head />
       <body>
         {/* To avoid FOUT with styled-components wrap Layout with StyledComponentsRegistry https://beta.nextjs.org/docs/styling/css-in-js#styled-components */}
-        <Layout>{children}</Layout>
+        <Layout>
+          {children}
+          <Analytics />
+        </Layout>
       </body>
     </html>
   )
